ed0bc4ab9cb08c15bf054449f1eca4be0cff16eb
Alin Ibadof <alinibadof@gmail.com>
2024-11-27 08:24:38 +0200

    add toggle between register and login

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -30,20 +30,55 @@ class Authentication extends LitElement {
     }
   `;
 
-  static properties = {};
+  static properties = {
+    isLoggedIn: { type: Boolean },
+    isRegisterScreen: { type: Boolean },
+    username: { type: String },
+    errorMessage: { type: String },
+  };
+
+  constructor() {
+    super();
+    this.isLoggedIn = false;
+    this.isRegisterScreen = false;
+    this.username = '';
+    this.errorMessage = '';
+  }
+
+  _toggleRegister() {
+    this.isRegisterScreen = !this.isRegisterScreen;
+    this.errorMessage = '';
+  }
 
   render() {
     return html`
       <main>
         <h3>Authentication</h3>
-        <p>Please login to continue</p>
+        <p>
+          ${this.isRegisterScreen
+            ? 'Please register to continue'
+            : 'Please login to continue'}
+        </p>
         <form>
           <lion-input label="Username" type="text"></lion-input>
           <lion-input label="Password" type="password"></lion-input>
         </form>
         <p>
-          Don't have an account? Click <button class="toggle">here</button> to
-          register.
+          ${this.isRegisterScreen
+            ? html`
+                Already have an account? Click
+                <button class="toggle" @click="${this._toggleRegister}">
+                  here
+                </button>
+                to login.
+              `
+            : html`
+                Don't have an account? Click
+                <button class="toggle" @click="${this._toggleRegister}">
+                  here
+                </button>
+                to register.
+              `}
         </p>
         <p></p>
       </main>
